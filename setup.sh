3e0ada20b54f6738af82e4072fe3bd757fbbfc9a
Gabriel Baquedano <g.baquedano.89@gmail.com>
2015-06-13 00:38:47 +0200

    Usar shell y no bash

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
 #!\bin\sh
-MODULE_EXPRESS=node_modules/express
-MODULE_I2C=node_modules/i2c
-MODULE_SERVESTATIC=node_modules/serve-static
-MODULE_HIREDIS=node_modules/hiredis
-MODULE_REDIS=node_modules/redis
-MODULE_IOREDIS=node_modules/ioredis
-MODULE_SOCKETIO=node_modules/socket.io
+MODULE[0]=node_modules/express
+MODULE[1]=node_modules/i2c
+MODULE[2]=node_modules/serve-static
+MODULE[3]=node_modules/hiredis
+MODULE[4]=node_modules/redis
+MODULE[5]=node_modules/ioredis
+MODULE[6]=node_modules/socket.io
 
-MODULES=(MODULE_EXPRESS MODULE_I2C MODULE_SERVESTATIC MODULE_HIREDIS MODULE_REDIS MODULE_IOREDIS MODULE_SOCKETIO)
-
-for i in "${arr[@]}"
+for i in "${MODULE[@]}"
 do
    echo "$i"
    # or do whatever with individual element of the array
